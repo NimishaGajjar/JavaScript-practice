733ac641b6ed678cfca23e4426f516b7ef405c80
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-09-20 14:47:35 -0400

    foor loop

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+const limit = 9; // Set the limit for the loop
+let total = 0;
+for (let i = 0; i <= limit; i++) {
+  total += i; // Add the value of i to the total on each iteration
+}
+console.log(total);
+
